3f0a7178dd1f4093e251c68f23c4097ee61ef4d3
tiago <obardolino@gmail.com>
2026-04-29 22:20:47 -0300

    atualizacoes

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.musgo.appvendas',
+  appName: 'App Vendas',
+  webDir: 'dist'
+};
+
+export default config;
